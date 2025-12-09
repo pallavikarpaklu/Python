b94d01c746866cad93c706005d89779013d0bb02
pallavikarpaklu <pallavikarpakluravi@gmail.com>
2025-12-08 23:32:02 -0800

    adding sort function

--- ListCPUProcess.py
+++ ListCPUProcess.py
@@ -1,7 +1,7 @@
 import os
 
 def list_processes():
-    processes = os.popen(' ps aux | head -n 10').read()
+    processes = os.popen(' ps aux | sort -k2 -r | head -n 100').read()
     print(processes)
 
 if __name__ == '__main__':
